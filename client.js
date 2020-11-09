4518cf5384c3ba84e5c8ca76b533fec17da2eb36
zuberabdikadir <67612000+zuberabdikadir@users.noreply.github.com>
2020-11-08 20:20:47 -0600

    inputed functions to js

--- client.js
+++ client.js
@@ -1,15 +1,12 @@
 console.log('Hello')
-let employee = [];
-let employeeList = $('#employee');
-
+let employees = [];
+let totalMonthlySalary = 0;
 
 $(document).ready(onReady);
 
 function onReady(){
     $( '#submitButton' ).on( 'click', employeeInput)
-
 }
-
 
 function employeeInput(){
     let firstName = $('#firstNameInput').val();
@@ -20,27 +17,54 @@ function employeeInput(){
     if ( firstName === '' || lastName === '' || id === '' || title === '' || annualSalary === ''){
         return;
     } else {
-        newEmployee(firstName, lastName, id, title, annualSalary);
-        console.log('employee', employee);
-        $('#employee').append(`<li>${firstName} ${lastName} ${id} ${title} ${annualSalary}</li>`)
+        newEmployee(firstName, lastName, id, title, Number(annualSalary));
+        console.log('employees', employees);
+        $('#employees').append(`<tr><td>${firstName}</td> <td>${lastName}</td> <td>${id}</td><td>${title}</td><td>$${annualSalary}</td><td><button onclick="deleteEmployee('${id}')">Delete</button></td></tr>`)
         $('#firstNameInput').val('');
         $('#lastNameInput').val('');
         $('#idInput').val('');
         $('#titleInput').val('');
         $('#annualSalaryInput').val('');
-
-
     }
 }
 
-function newEmployee(firstNameInput, lastNameInput, idInput, titleInput, annualSalary){
-    const newEmployeeObject ={
+function newEmployee(firstNameInput, lastNameInput, idInput, titleInput, annualSalaryInput){
+    let newEmployeeObject ={
         firstName: firstNameInput,
         lastName: lastNameInput,
         id: idInput,
         title: titleInput,
         annualSalary: annualSalaryInput
     }
-    employee.push(newEmployeeObject);
+    employees.push(newEmployeeObject);
+    updateMonthlySalary(annualSalaryInput);
+}
+
+function deleteEmployee(id) {
+    let employeeTable = $('#employees')[0];
+    let employeeIndex = getEmployeeIndex(id);
+    let employee = employees[employeeIndex];
+    employees.splice(employeeIndex, 1);
+    employeeTable.children[employeeIndex + 1].remove();
+    updateMonthlySalary(employee.annualSalary * -1);
+}
+
+function getEmployeeIndex(id){
+    for(let i = 0; i < employees.length; i++) {
+        if(employees[i].id === id) {
+            return i;
+        }
+    }
+}
+
+function updateMonthlySalary(yearlySalary) {
+    totalMonthlySalary += yearlySalary / 12;
+    $('#total-monthly').text(`$${totalMonthlySalary}`);
+
+    if(totalMonthlySalary > 20000) {
+        $('#total-monthly-salary').css('background-color', 'red');
+    } else {
+        $('#total-monthly-salary').css('background-color', 'transparent');
+    }
 
 }
